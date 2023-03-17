0beea369d314d088115833c7d4101b94970a78fd
mitsuonitta <infinias320@gmail.com>
2023-03-17 15:55:21 +0900

    ユーザー管理機能 commit09

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,8 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX
    with_options presence: true do
    validates :nickname, presence: true
-   validates :first_name, :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
+   validates :first_name, :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
    validates :first_name_kana, :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
    validates :birth_date, presence: true
  end
 end
-#validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze}
-#validates :nickname, presence: true
-#validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
-#validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
-#validates :last_name_kana, presence: true, format:  { with: /\A[ァ-ヶー－]+\z/}
-#validates :first_name_kana, presence: true, format:  { with: /\A[ァ-ヶー－]+\z/}
-#validates :birthday, presence: true
-#end
