8ca0c46b74217a7889f49668c23c91bb68c70cbc
mitsuonitta <infinias320@gmail.com>
2023-03-18 18:19:01 +0900

    商品種品機能 commit01

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :set_furima, only: [:edit, :update, :destroy]
+  before_action :prevent_url, only: [:edit, :update, :destroy]
+
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  # def destroy
+  # end
+
   def index
+    @items = item.new
   end
+
+  # def new
+  # end
+  #private
+
+
+    # def set_furima
+    #   @item = Item.find(params[:id])
+    # end
+
+    # def prevent_url
+    # if @item.user_id != current_user.id || @item.purchase != nil #　コードを追加
+    #   redirect_to root_path
+    # end
+  # end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,17 +3,17 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_one_attached :image
-  #has_one :orders
+  has_one :orders
 
-  #with_options presence: true do
-  #validates :name
-  #validates :price
-  #validates :description
-  #validates :preparation_day_id, numericality: { other_than: 1 }
+  with_options presence: true do
+  validates :name
+  validates :price
+  validates :description
+  validates :preparation_day_id, numericality: { other_than: 1 }
   validates :category_id, numericality: { other_than: 1 }
-  #validates :user
-  #validates :prefecture_id, numericality: { other_than: 1 }
-  #validates :item_condition_id, numericality: { other_than: 1 }
-  #validates :postage_payer_id, numericality: { other_than: 1 }
-  #end
+  validates :user
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :item_condition_id, numericality: { other_than: 1 }
+  validates :postage_payer_id, numericality: { other_than: 1 }
+  end
 end
