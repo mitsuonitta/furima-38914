e62765ca00eaa0fcf2ada4a9cae726307607bb1a
mitsuonitta <infinias320@gmail.com>
2023-03-19 12:12:28 +0900

    商品出品機能 commit04

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,25 +2,47 @@ class ItemsController < ApplicationController
   before_action :set_furima, only: [:edit, :update, :destroy]
   before_action :prevent_url, only: [:edit, :update, :destroy]
 
-  # def edit
-  # end
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def create
+    @item = Item.new #(item_params)
+     if @item.save
+       redirect_to root_path
+     else
+       render :new
+     end
+  end
 
   # def update
-  # end
-
-  # def destroy
+  #   if @items.update(items_params)
+  #     redirect_to items_path(@prototype)
+  #   else
+  #     render :edit
+  #   end
   # end
 
   def index
-   @items = Item.new
+    @item = Item.new
   end
 
-  # def new
-  #   @items = item.new
+  def new
+    @item = Item.new
+  end
+  
+  # def destroy
+  #   if @items.destroy
+  #     redirect_to root_path
+  #   else
+  #     redirect_to root_path
+  #   end
   # end
-  #private
+  private
 
-
+  # def item_params
+  #   params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  # end
     # def set_furima
     #   @item = Item.find(params[:id])
     # end
