bc1f42f0db86211ca580af4d13b6fcc131fa625f
mitsuonitta <infinias320@gmail.com>
2023-03-23 22:45:03 +0900

    詳細表示機能 commit01

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,28 +1,15 @@
 class ItemsController < ApplicationController
   # before_action :set_furima, only: [:edit, :update, :destroy]
   # before_action :prevent_url, only: [:edit, :update, :destroy]
-  before_action :set_item, only: [:edit, :show]
-  before_action :authenticate_user!, except: [:show, :index]
-  before_action :move_to_index, except: [:index, :show]
-  #validates :content, presence: true, unless: :was_attached?
+  before_action :authenticate_user!, except: [:index]
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
+  # before_action :move_to_index, except: [:index, :show]
+  # validates :content, presence: true, unless: :was_attached?
 
   def index
     @item = Item.all.order("created_at DESC")
-  end
 
-  def show
   end
-
-  def edit
-  end
-
-  def set_item
-    @item = Item.find(params[:id])
-  end
-
-  # def was_attached?
-  #   self.image.attached?
-  # end
 
   def new
     @item = Item.new
@@ -32,42 +19,58 @@ class ItemsController < ApplicationController
     @item = Item.new (item_params)
      if @item.save
        redirect_to root_path
-     else
+    else
        render :new
      end
   end
 
-  def move_to_index
-    redirect_to new_user_session_path unless user_signed_in?
+  def edit
   end
 
-  # def update
-  #  if @items.update(items_params)
-  # #     redirect_to items_path(@prototype)
-  # #   else
-  # #     render :edit
-  #    end
+  # def was_attached?
+  #   self.image.attached?
   # end
-  
-  # def destroy
-  #   if @items.destroy
-  #     redirect_to root_path
-  #   else
-  #     redirect_to root_path
-  #   end
+
+  # def move_to_index
+  #   redirect_to new_user_session_path unless user_signed_in?
   # end
+
+  def update
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    if current_user.id == @item.user_id
+    if @items.destroy
+    end 
+      redirect_to root_path
+   end
+  end
+
   private
 
   def item_params
-     params.require(:item).permit(:image, :name, :description, :category_id, :item_condition_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :price).merge(user_id: current_user.id)
+     params.require(:item).permit(:image, :name, :description, :category_id, :item_condition_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :price, :user).merge(user_id: current_user.id)
   end
-    # def set_furima
-    #   @item = Item.find(params[:id])
-    # end
 
-    # def prevent_url
-    # if @item.user_id != current_user.id || @item.purchase != nil #　コードを追加
-    #   redirect_to root_path
-    # end
-  #end
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  # def set_furima
+  #    @item = Item.find(params[:id])
+  # end
+  # def prevent_url
+  # if @item.user_id != current_user.id || @item.purchase != nil #　コードを追加
+  #   redirect_to root_path
+  # params {A: aa, item:{name: jj} ,B:b}
+  # param.require(:item).permit(:name)
+  # end
+  # end
 end
