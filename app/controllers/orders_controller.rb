9cadedfee2de16db8fd5d2d69a8205649f04a4cf
mitsuonitta <infinias320@gmail.com>
2023-03-26 18:52:17 +0900

    購入機能 commit01

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,42 +1,30 @@
 class OrdersController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, except: :index
 
-    def index
-      @item = Item.find(params[:item_id])
-    end
+  def index
+    @item = Item.find(params[:item_id])
+    @order_addresse = OrderAddresse.new
+  end
   
-    def new
-      @item = Item.find(params[:item_id])
-      @cart_address = CartAddress.new
-    end
+  def new
+    @order_addresse = OrderAddresse.new
+  end
   
-    def create
-      @cart_address = CartAddress.new(cart_params)
-      if @cart_address.valid?
-        @cart_address.save
-        redirect_to root_path
-      else
-        render template: "order/index"
-      end
+  def create
+    @item = Item.find(params[:item_id])
+    @order_addresse = OrderAddresse.new(order_addresse)
+    if @order_addresse.valid?
+      @order_addresse.save
+      redirect_to root_path
+    else
+      render :index
     end
+  end
+
 
    private
 
-   def order_params
-     params.require(:order).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+   def order_addresse
+    params.require(:order_addresse).permit(:order_id, :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number). merge(user_id: current_user.id, item_id: params[:item_id])
    end
-
-  #  def pay_item
-  #    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-  #    Payjp::Charge.create(
-  #      amount: @item.price,        # 商品の値段
-  #      card: order_params[:token], # カードトークン
-  #      currency: 'jpy'             # 通貨の種類（日本円）
-  #    )
-  #  end
-
-
-    #  @item = Item.find(params[:item_id])
-    #  redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
-    #end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,25 +1,6 @@
 class Order < ApplicationRecord
    include ActiveModel::Model
-   #attr_accessor  :user_id, :item_id
    belongs_to :users
    has_one :address
-
-   with_options presence: true do
-     validates :user_id
-     validates :item_id
-     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-     validates :city
-     validates :house_number
-     validates :building_name
-     validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
-
-     #validates :token
-   end
-
-   def save
-     order = Order.create(user_id: user_id, item_id: item_id)
-     Payment.create(order_id: order.id, post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)
-   end
 end
 
